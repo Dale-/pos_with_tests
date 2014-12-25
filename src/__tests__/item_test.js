11a86b1d1a9f3b8df736b6863c768b205ba49eb2
dale <18717394542@163.com>
2014-12-25 21:15:45 +0800

    create test file as item_test.js for test Item.all()

---
+++ src/__tests__/item_test.js
@@ -0,0 +1,25 @@
+jest.dontMock('../model/item');
+
+describe('Item', function(){
+
+  describe('.all()', function(){
+
+    it('should return correct string',function(){
+
+      var Item = require('../model/item');
+
+      var result = Item.all();
+
+      var expectBarcode = 'ITEM000000';
+      var expectName = '雪碧';
+      var expectUnit = '斤';
+      var expectPrice = 15.00;
+
+      expect(result[0].barcode).toBe(expectBarcode);
+      expect(result[1].name).toBe(expectName);
+      expect(result[2].unit).toBe(expectUnit);
+      expect(result[3].price).toBe(expectPrice);
+    });
+
+  });
+});
