dece3660594bbb1c63261f9e9b0c311085b34574
dale <18717394542@163.com>
2014-12-26 07:27:40 +0800

    create util_test.js file to test Util.toFixed()

---
+++ src/__tests__/util_test.js
@@ -0,0 +1,17 @@
+jest.dontMock('../model/util');
+
+describe('Util', function(){
+
+  describe('.toFixed()', function(){
+
+    it('should return correct promotion num format',function(){
+
+      var Util = require('../model/util');
+
+      var result = Util.toFixed(2.2345);
+
+      expect(result).toBe('2.23');
+    });
+
+  });
+});
